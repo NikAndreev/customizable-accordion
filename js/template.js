9e45736c4335e606baef19213560b62c7bce245e
NikAndreev <57640150+NikAndreev@users.noreply.github.com>
2021-02-20 18:00:28 +0300

    Update template.js

--- js/template.js
+++ js/template.js
@@ -1,34 +1,40 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-	let accordionItems = document.querySelectorAll(".accordion__item");
-	let accordionHeaders = document.querySelectorAll(".accordion__header");
+	let accordions = document.querySelectorAll(".accordion");
 
-	accordionItems.forEach( function(accordionItem){
-		switchAccordionBody(accordionItem);
-	});
+	accordions.forEach( function(accordion){
 
-	accordionHeaders.forEach( function(accordionHeader){
-		accordionHeader.addEventListener("click", function(event){
+		let accordionItems = accordion.querySelectorAll(".accordion__item");
+		let accordionHeaders = accordion.querySelectorAll(".accordion__header");
 
-			accordionItems.forEach( function(accordionItem){
-				if (accordionItem.contains(event.target)) {
-					accordionItem.classList.toggle("active");
-				} else {
-					accordionItem.classList.remove("active");
-				}
-
-				switchAccordionBody(accordionItem);
-			});
-
+		accordionItems.forEach( function(accordionItem){
+			switchAccordionBody(accordionItem);
 		});
-	});
 
-	function switchAccordionBody(accordionItem) {
-		if (accordionItem.classList.contains("active")) {
-			accordionItem.lastElementChild.style.height = accordionItem.lastElementChild.scrollHeight + "px";
-		} else {
-			accordionItem.lastElementChild.style.height = "0px";
+		accordionHeaders.forEach( function(accordionHeader){
+			accordionHeader.addEventListener("click", function(event){
+
+				accordionItems.forEach( function(accordionItem){
+					if (accordionItem.contains(event.target)) {
+						accordionItem.classList.toggle("active");
+					} else {
+						accordionItem.classList.remove("active");
+					}
+
+					switchAccordionBody(accordionItem);
+				});
+
+			});
+		});
+
+		function switchAccordionBody(accordionItem) {
+			if (accordionItem.classList.contains("active")) {
+				accordionItem.lastElementChild.style.height = accordionItem.lastElementChild.scrollHeight + "px";
+			} else {
+				accordionItem.lastElementChild.style.height = "0px";
+			}
 		}
-	}
+
+	});
 
 });
