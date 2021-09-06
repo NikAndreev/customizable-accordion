7318a9563adcddc14c5ec013f692939cfdfb9f37
Nikita <nik.andreev.99@yandex.ru>
2021-09-06 21:31:18 +0300

    updated css

--- js/template.js
+++ js/template.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener('DOMContentLoaded', function(){
 
-	let accordionGroup = document.querySelectorAll(".js-accordion");
+	let accordionGroup = document.querySelectorAll('.js-accordion');
 
 	accordionGroup.forEach( function(accordion) {
 
-		let accordionItemGroup = accordion.querySelectorAll(".js-accordion-item");
-		let accordionHeaderGroup = accordion.querySelectorAll(".js-accordion-header");
+		let accordionItemGroup = accordion.querySelectorAll('.js-accordion-item');
+		let accordionHeaderGroup = accordion.querySelectorAll('.js-accordion-header');
 
 		let accordionItemFirst = accordionItemGroup[0];
-		accordionItemFirst.classList.add("active");
+		accordionItemFirst.classList.add('active');
 
 		accordionItemGroup.forEach( accordionItem => {
 			switchItemBody.apply(accordionItem);
 		});
 
 		accordionHeaderGroup.forEach( accordionHeader => {
-			accordionHeader.addEventListener("click", event => {
+			accordionHeader.addEventListener('click', event => {
 
 				accordionItemGroup.forEach( accordionItem => {
 					
-					accordionItem.contains(event.currentTarget) ? accordionItem.classList.toggle("active") : accordionItem.classList.remove("active");
+					accordionItem.contains(event.currentTarget) ? accordionItem.classList.toggle('active') : accordionItem.classList.remove('active');
 
 					switchItemBody.apply(accordionItem);
 				});
@@ -28,11 +28,11 @@ document.addEventListener("DOMContentLoaded", function(){
 		});
 
 		function switchItemBody() {
-			let accordionBody = this.querySelector(".js-accordion-body");
-			if (this.classList.contains("active")) {
-				accordionBody.style.height = accordionBody.scrollHeight + "px";
+			let accordionBody = this.querySelector('.js-accordion-body');
+			if (this.classList.contains('active')) {
+				accordionBody.style.height = accordionBody.scrollHeight + 'px';
 			} else {
-				accordionBody.style.height = "0px";
+				accordionBody.style.height = '0px';
 			}
 		}
 
