3edd196cd6718f515d34fa436061db1ecbc2dcf8
Nikita <nik.andreev.99@yandex.ru>
2022-05-01 16:16:52 +0300

    add class Accordion

--- js/template.js
+++ js/template.js
@@ -1,20 +1,91 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-	const accordionGroup = document.querySelectorAll('.accordion');
+	class Accordion {
+		#accordionEl
+		#itemsEl
+		#control
+		#container
+		#content
+		#activeClass
+		#mode
+		#animation
 
-	accordionGroup.forEach( accordion => {
+		constructor(accordion, config) {
+			this.#accordionEl = accordion
+			this.#itemsEl = this.#accordionEl.querySelectorAll(config.item)
+			this.#control = config.control
+			this.#container = config.container
+			this.#content = config.content
+			this.#activeClass = config.activeClass
+			this.#mode = config.mode
+			this.#animation = config.animation
 
-		const accordionItemGroup = accordion.querySelectorAll('.accordion__item');
-		accordionItemGroup[0].classList.add('active');
+			this.#accordionEl.addEventListener('click', event => {
+				if (event.target.closest(this.#control)) {
+					switch (this.#mode) {
+						case 'collapsible':
+							this.#collapsible(event)
+							break
+						case 'multiple':
+							this.#multiple(event)
+							break
+						default:
+							this.#default(event)
+					}
 
-		accordion.addEventListener('click', event => {
-			if (event.target.closest('.accordion__header')) {
-				accordionItemGroup.forEach( accordionItem => {	
-					accordionItem.contains(event.target) ? accordionItem.classList.toggle('active') : accordionItem.classList.remove('active');
-				});
+					if (this.#animation) {
+						this.#animate()
+					}
+				}
+			})
+			
+			if (this.#animation) {
+				this.#animate()
+
+				window.addEventListener('resize', () => {
+					this.#animate()
+				})
 			}
-		});
 
-	});
+		}
 
-});
+		#collapsible(event) {
+			this.#itemsEl.forEach(item => {
+				item.contains(event.target) ? item.classList.toggle(this.#activeClass) : item.classList.remove(this.#activeClass)
+			})
+		}
+
+		#multiple(event) {
+			this.#itemsEl.forEach(item => {
+				item.contains(event.target) ? item.classList.toggle(this.#activeClass) : ''
+			})
+		}
+
+		#default(event) {
+			this.#itemsEl.forEach(item => {
+				item.contains(event.target) ? item.classList.add(this.#activeClass) : item.classList.remove(this.#activeClass)
+			})
+		}
+
+		#animate() {
+			this.#itemsEl.forEach(item => {
+				item.querySelector(this.#container).style.height = item.classList.contains(this.#activeClass) ? item.querySelector(this.#content).scrollHeight + 'px' : ''
+			})
+		}
+	}
+
+	const config = {
+		item: '[data-item]',
+		control: '[data-control]',
+		container: '[data-container]',
+		content: '[data-content]',
+		activeClass: 'active',
+		mode: 'collapsible',
+		animation: true
+	}
+
+	document.querySelectorAll('[data-accordion]').forEach( accordion => {
+		new Accordion(accordion, config)
+	})
+
+})
