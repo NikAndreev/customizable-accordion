0369049d80555454367e8b4c71864c22683e6da5
Nikita <nik.andreev.99@yandex.ru>
2022-05-05 22:52:36 +0300

    protected fields

--- js/template.js
+++ js/template.js
@@ -1,75 +1,66 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 	class Accordion {
-		#accordionEl
-		#itemsEl
-		#control
-		#container
-		#content
-		#activeClass
-		#mode
-		#animation
-
 		constructor(accordion, config) {
-			this.#accordionEl = accordion
-			this.#itemsEl = this.#accordionEl.querySelectorAll(config.item)
-			this.#control = config.control
-			this.#container = config.container
-			this.#content = config.content
-			this.#activeClass = config.activeClass
-			this.#mode = config.mode
-			this.#animation = config.animation
+			this._accordionEl = accordion
+			this._itemsEl = this._accordionEl.querySelectorAll(config.item)
+			this._control = config.control
+			this._container = config.container
+			this._content = config.content
+			this._activeClass = config.activeClass
+			this._mode = config.mode
+			this._animation = config.animation
 
-			this.#accordionEl.addEventListener('click', event => {
-				if (event.target.closest(this.#control)) {
-					switch (this.#mode) {
+			this._animate = this._animate.bind(this)
+
+			this._accordionEl.addEventListener('click', event => {
+				if (event.target.closest(this._control)) {
+					switch (this._mode) {
 						case 'collapsible':
-							this.#collapsible(event)
+							this._collapsible(event)
 							break
 						case 'multiple':
-							this.#multiple(event)
+							this._multiple(event)
 							break
 						default:
-							this.#default(event)
+							this._default(event)
 					}
 
-					if (this.#animation) {
-						this.#animate()
+					if (this._animation) {
+						this._animate()
 					}
 				}
 			})
 			
-			if (this.#animation) {
-				this.#animate()
+			if (this._animation) {
+				this._animate()
 
-				window.addEventListener('resize', () => {
-					this.#animate()
-				})
+				window.addEventListener('resize', this._animate)
 			}
 
 		}
 
-		#collapsible(event) {
-			this.#itemsEl.forEach(item => {
-				item.contains(event.target) ? item.classList.toggle(this.#activeClass) : item.classList.remove(this.#activeClass)
+		_collapsible(event) {
+			this._itemsEl.forEach(item => {
+				item.contains(event.target) ? item.classList.toggle(this._activeClass) : item.classList.remove(this._activeClass)
 			})
 		}
 
-		#multiple(event) {
-			this.#itemsEl.forEach(item => {
-				item.contains(event.target) ? item.classList.toggle(this.#activeClass) : ''
+		_multiple(event) {
+			this._itemsEl.forEach(item => {
+				item.contains(event.target) ? item.classList.toggle(this._activeClass) : ''
 			})
 		}
 
-		#default(event) {
-			this.#itemsEl.forEach(item => {
-				item.contains(event.target) ? item.classList.add(this.#activeClass) : item.classList.remove(this.#activeClass)
+		_default(event) {
+			this._itemsEl.forEach(item => {
+				item.contains(event.target) ? item.classList.add(this._activeClass) : item.classList.remove(this._activeClass)
 			})
 		}
 
-		#animate() {
-			this.#itemsEl.forEach(item => {
-				item.querySelector(this.#container).style.height = item.classList.contains(this.#activeClass) ? item.querySelector(this.#content).scrollHeight + 'px' : ''
+		_animate() {
+			this._itemsEl.forEach(item => {
+				item.querySelector(this._container).style.height = item.classList.contains(this._activeClass) ? item.querySelector(this._content).scrollHeight + 'px' : ''
 			})
 		}
 	}
